e0f960c3b515efb692de2b83c0dbc8f08e411b26
Panda-Warrior3 <brilmansander@gmail.com>
2022-07-22 14:32:23 +0200

    updated redirects.php comment

--- assets/php/redirects.php
+++ assets/php/redirects.php
@@ -7,7 +7,7 @@ function redirect(string $url)
     /**
      * Redirect to a new page.
      * 
-     * @param string The path from the website url.
+     * @param string The path from the root website url.
      * 
      * @return void You will never reach this code anyways so it doesnt matter
      */
