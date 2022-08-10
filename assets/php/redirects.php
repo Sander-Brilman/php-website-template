72d08a867b549a46987ba70da3598c45ac6bdfa0
Panda-Warrior3 <brilmansander@gmail.com>
2022-08-10 20:05:09 +0200

    fixed trailing / remover in redirects.php

--- assets/php/redirects.php
+++ assets/php/redirects.php
@@ -18,17 +18,16 @@
  * - @link https://github.com/Sander-Brilman/php-website-template#how-to-use
  */
 
-
 // remove trailing '/' from url to improve SEO
 if (end($url_array) == '') {
     $url            = str_replace($site_folder, '', $_SERVER['REQUEST_URI']);
     $param_location = strpos($url, '?');
-    redirect(
-    $param_location === false 
-        ? substr($url,0,strlen($url)-1) 
-        : substr($url,0,$param_location-1).substr($url,$param_location)
-    );
+    if ($url !== '') {
+        redirect(
+            $param_location === false 
+                ? substr($url,0,strlen($url)-1) 
+                : substr($url,0,$param_location-1).substr($url,$param_location)
+        );
+    }
 }
-
-
 ?>
